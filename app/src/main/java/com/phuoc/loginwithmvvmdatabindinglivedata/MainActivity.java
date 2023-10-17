c18d98262320f782c6e52c2702fd7c6f1cca17dd
Nguyễn Đức Phước <nguyenducphuoc0802@gmail.com>
2023-10-17 23:34:09 +0700

    Initial commit

---
+++ app/src/main/java/com/phuoc/loginwithmvvmdatabindinglivedata/MainActivity.java
@@ -0,0 +1,35 @@
+package com.phuoc.loginwithmvvmdatabindinglivedata;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.phuoc.loginwithmvvmdatabindinglivedata.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        ActivityMainBinding binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        LoginViewModel loginViewModel = new ViewModelProvider(this).get(LoginViewModel.class);
+        binding.setLoginViewModel(loginViewModel);
+        binding.setLifecycleOwner(this);
+
+        loginViewModel.getUser().observe(this, new Observer<User>() {
+            @Override
+            public void onChanged(@Nullable User user) {
+                if (user.getEmail().length() > 0 && user.getPassword().length() > 0)
+                    Toast.makeText(getApplicationContext(), "email : " + user.getEmail() + " password " + user.getPassword(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+}
